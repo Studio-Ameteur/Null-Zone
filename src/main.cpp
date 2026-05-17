e056d2b4b2eeea0c8778facec7b7bdd6237133fe
Amateur Programmer <solo2244rt@gmail.com>
2026-05-17 18:16:51 +0700

    Refactor main.cpp for improved variable management

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,35 @@
 #include "include/savegame.h"
 #include "include/menu.h"
 
-#ifdef _WIN32
-#pragma comment(linker, "/subsystem:windows")
-#endif
+static GameState g_state      = GS_MENU;
+static bool      g_running    = true;
+static bool      g_fullscreen = false;
+static double    g_fps        = 0.0;
+static double    g_frametime  = 0.0;
+static bool      g_mouse_captured = false;
+static SDL_Window*   g_window   = nullptr;
+static SDL_Renderer* g_renderer = nullptr;
 
-static GameState  g_state      = GS_MENU;
-static bool       g_running    = true;
-static double     g_fps        = 0.0;
-static double     g_frametime  = 0.0;
-static bool       g_footstep   = false;
+static void mouse_capture(bool capture) {
+    g_mouse_captured = capture;
+    SDL_SetRelativeMouseMode(capture ? SDL_TRUE : SDL_FALSE);
+    SDL_ShowCursor(capture ? SDL_DISABLE : SDL_ENABLE);
+}
 
-static TTF_Font* load_font_fallback(int size) {
+static void toggle_fullscreen() {
+    g_fullscreen = !g_fullscreen;
+    if (g_fullscreen)
+        SDL_SetWindowFullscreen(g_window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    else
+        SDL_SetWindowFullscreen(g_window, 0);
+}
+
+static TTF_Font* load_font(int size) {
     const char* paths[] = {
         "C:\\Windows\\Fonts\\cour.ttf",
+        "C:\\Windows\\Fonts\\courbd.ttf",
         "C:\\Windows\\Fonts\\arial.ttf",
+        "C:\\Windows\\Fonts\\arialbd.ttf",
         "C:\\Windows\\Fonts\\verdana.ttf",
         "C:\\Windows\\Fonts\\tahoma.ttf",
         nullptr
@@ -56,19 +71,18 @@ static void game_new() {
 }
 
 static void game_next_level() {
-    int next = g_player.level + 1;
-    if (next > MAX_LEVELS) next = MAX_LEVELS;
-    int score  = g_player.score;
-    int kills  = g_player.kills;
+    int next    = std::min(g_player.level + 1, MAX_LEVELS);
+    int score   = g_player.score;
+    int kills   = g_player.kills;
     Weapon weapons[MAX_WEAPONS];
-    for (int i=0;i<MAX_WEAPONS;i++) weapons[i]=g_player.weapons[i];
-    int curWpn = g_player.currentWeapon;
+    for (int i=0;i<MAX_WEAPONS;i++) weapons[i] = g_player.weapons[i];
+    int curWpn  = g_player.currentWeapon;
 
     game_start_level(next);
 
     g_player.score = score;
     g_player.kills = kills;
-    for (int i=0;i<MAX_WEAPONS;i++) g_player.weapons[i]=weapons[i];
+    for (int i=0;i<MAX_WEAPONS;i++) g_player.weapons[i] = weapons[i];
     g_player.currentWeapon = curWpn;
 }
 
@@ -78,28 +92,27 @@ static void handle_shoot() {
 
     audio_play(weapon_sound(g_player.currentWeapon));
 
-    int wpn   = g_player.currentWeapon;
-    int dmg   = weapon_damage[wpn];
-    double sp = weapon_spread[wpn];
-
-    int pellets = (wpn==(int)WPN_SHOTGUN)      ? 7  :
-                  (wpn==(int)WPN_SUPER_SHOTGUN) ? 14 : 1;
+    int    wpn     = g_player.currentWeapon;
+    int    dmg     = weapon_damage[wpn];
+    double sp      = weapon_spread[wpn];
+    int    pellets = (wpn==(int)WPN_SHOTGUN)       ? 7  :
+                     (wpn==(int)WPN_SUPER_SHOTGUN) ? 14 : 1;
 
     for (int pel=0; pel<pellets; pel++) {
         double angle = g_player.angle;
         if (sp > 0) {
-            double r = ((double)(rand()%10000)/10000.0 - 0.5) * 2.0 * sp;
+            double r = ((double)(rand()%10000)/10000.0 - 0.5)*2.0*sp;
             angle += r;
         }
         Vec2 dir = {cos(angle), sin(angle)};
-        int pelDmg = (pellets>1) ? dmg/pellets : dmg;
+        int pelDmg = pellets>1 ? dmg/pellets : dmg;
 
         if (wpn==(int)WPN_CHAINSAW || wpn==(int)WPN_FISTS) {
             for (int i=0;i<g_entityCount;i++) {
                 Entity& e=g_entities[i];
                 if (!e.active||e.type!=ENT_ENEMY||e.state==ES_DEAD) continue;
-                double dist=e.pos.distTo(g_player.pos);
-                if (dist<1.5) entity_take_damage(i, dmg, g_player);
+                if (e.pos.distTo(g_player.pos)<1.5)
+                    entity_take_damage(i, dmg, g_player);
             }
         } else if (wpn==(int)WPN_BFG) {
             for (int i=0;i<g_entityCount;i++) {
@@ -108,40 +121,19 @@ static void handle_shoot() {
                 if (e.inAggro) entity_take_damage(i, dmg, g_player);
             }
         } else {
-            EntityHitResult hit = entities_raycast_hit(
-                g_player.pos, dir, 30.0, pelDmg);
+            EntityHitResult hit = entities_raycast_hit(g_player.pos, dir, 30.0, pelDmg);
             if (hit.hit) {
                 entity_take_damage(hit.entityIdx, hit.damage, g_player);
-                audio_play_3d(SND_ENEMY_DEATH,
-                    g_entities[hit.entityIdx].pos, g_player.pos);
+                audio_play_3d(SND_ENEMY_DEATH, g_entities[hit.entityIdx].pos, g_player.pos);
             }
         }
     }
 }
 
 static void update_game(double dt) {
-    input_begin_frame();
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-        if (input_process(e)) { g_running=false; return; }
-    }
-
-    if (action_debug()) hud_toggle_debug();
-    if (action_map())   hud_toggle_map();
-
-    if (action_pause()) {
-        menu_open_pause(GS_PLAYING);
-        g_state = GS_PAUSED;
-        SDL_SetRelativeMouseMode(SDL_FALSE);
-        return;
-    }
-
-    player_update(g_player, dt, g_footstep);
-
-    if (g_footstep) {
-        SoundID ss = step_sound(g_player.pos);
-        audio_play(ss, 0.4);
-    }
+    bool footstep = false;
+    player_update(g_player, dt, footstep);
+    if (footstep) audio_play(step_sound(g_player.pos), 0.4);
 
     handle_shoot();
 
@@ -155,7 +147,7 @@ static void update_game(double dt) {
     if (!g_player.alive) {
         audio_play(SND_PLAYER_DEATH);
         g_state = GS_DEAD;
-        SDL_SetRelativeMouseMode(SDL_FALSE);
+        mouse_capture(false);
         return;
     }
 
@@ -164,38 +156,41 @@ static void update_game(double dt) {
         audio_play(SND_LEVEL_COMPLETE);
         savegame_autosave(g_player);
         g_state = GS_LEVEL_COMPLETE;
-        SDL_SetRelativeMouseMode(SDL_FALSE);
+        mouse_capture(false);
     }
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) < 0) return 1;
 
-    SDL_Window* window = SDL_CreateWindow(
+    g_window = SDL_CreateWindow(
         NZ_TITLE " v" NZ_VERSION,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN);
-    if (!window) { SDL_Quit(); return 1; }
+        SCREEN_W, SCREEN_H,
+        SDL_WINDOW_SHOWN|SDL_WINDOW_INPUT_FOCUS);
+    if (!g_window) { SDL_Quit(); return 1; }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+    g_renderer = SDL_CreateRenderer(g_window, -1,
         SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-    if (!renderer) { SDL_DestroyWindow(window); SDL_Quit(); return 1; }
+    if (!g_renderer) { SDL_DestroyWindow(g_window); SDL_Quit(); return 1; }
 
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    SDL_SetRelativeMouseMode(SDL_FALSE);
+    SDL_SetRenderDrawBlendMode(g_renderer, SDL_BLENDMODE_BLEND);
+    SDL_RaiseWindow(g_window);
 
     hud_init();
-    g_font_large  = load_font_fallback(28);
-    g_font_medium = load_font_fallback(20);
-    g_font_small  = load_font_fallback(14);
+    g_font_large  = load_font(28);
+    g_font_medium = load_font(20);
+    g_font_small  = load_font(14);
 
     textures_init();
-    renderer_init(renderer);
+    renderer_init(g_renderer);
     audio_init();
     input_init();
     savegame_init_dirs();
     savegame_load_registry();
     menu_init();
+
+    mouse_capture(false);
 
     Uint64 perfFreq    = SDL_GetPerformanceFrequency();
     Uint64 prevTime    = SDL_GetPerformanceCounter();
@@ -208,7 +203,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         Uint64 now   = SDL_GetPerformanceCounter();
         double delta = (double)(now - prevTime) / (double)perfFreq;
         prevTime     = now;
-        if (delta > 0.05) delta = 0.05;
+        if (delta > 0.1) delta = 0.1;
         g_frametime  = delta;
         fpsTimer    += delta;
         frameCount++;
@@ -221,17 +216,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         input_begin_frame();
         SDL_Event ev;
         while (SDL_PollEvent(&ev)) {
-            if (input_process(ev)) g_running=false;
+            if (ev.type == SDL_QUIT) { g_running = false; break; }
+            if (ev.type == SDL_WINDOWEVENT) {
+                if (ev.window.event == SDL_WINDOWEVENT_FOCUS_LOST)
+                    mouse_capture(false);
+                if (ev.window.event == SDL_WINDOWEVENT_FOCUS_GAINED && g_state==GS_PLAYING)
+                    mouse_capture(true);
+            }
+            input_process(ev);
         }
 
-        switch(g_state) {
+        if (key_pressed(SDL_SCANCODE_F5)) toggle_fullscreen();
+        if (action_debug()) hud_toggle_debug();
+        if (action_map())   hud_toggle_map();
+
+        switch (g_state) {
         case GS_MENU:
         case GS_PAUSED: {
             MenuAction act = menu_update(g_player, g_state);
-            switch(act) {
+            switch (act) {
             case MACT_NEW_GAME:
-                SDL_SetRelativeMouseMode(SDL_TRUE);
                 game_new();
+                mouse_capture(true);
                 break;
             case MACT_CONTINUE:
             case MACT_LOAD_OK:
@@ -239,25 +245,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                 entities_spawn_for_level(g_player.level, g_world.seed);
                 world_update_room_states(g_player.pos);
                 audio_play_music(g_player.level);
-                SDL_SetRelativeMouseMode(SDL_TRUE);
-                g_state=GS_PLAYING;
+                g_state = GS_PLAYING;
+                mouse_capture(true);
                 break;
             case MACT_RESUME:
-                SDL_SetRelativeMouseMode(SDL_TRUE);
-                g_state=GS_PLAYING;
+                g_state = GS_PLAYING;
+                mouse_capture(true);
                 break;
             case MACT_GOTO_MAIN:
-                SDL_SetRelativeMouseMode(SDL_FALSE);
-                g_state=GS_MENU;
+                g_state = GS_MENU;
+                mouse_capture(false);
                 break;
             case MACT_QUIT:
-                g_running=false;
+                g_running = false;
                 break;
             default: break;
             }
             break;
         }
         case GS_PLAYING: {
+            if (action_pause()) {
+                menu_open_pause(GS_PLAYING);
+                g_state = GS_PAUSED;
+                mouse_capture(false);
+                break;
+            }
             accumulator += delta;
             while (accumulator >= fixedDt) {
                 update_game(fixedDt);
@@ -268,52 +280,54 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         }
         case GS_DEAD: {
             if (key_pressed(SDL_SCANCODE_R)) {
-                SDL_SetRelativeMouseMode(SDL_TRUE);
                 game_new();
+                mouse_capture(true);
             }
             if (action_pause()) {
-                g_state=GS_MENU;
+                g_state = GS_MENU;
                 menu_open_main();
+                mouse_capture(false);
             }
             break;
         }
         case GS_LEVEL_COMPLETE: {
             if (key_pressed(SDL_SCANCODE_RETURN)||key_pressed(SDL_SCANCODE_KP_ENTER)) {
-                SDL_SetRelativeMouseMode(SDL_TRUE);
                 game_next_level();
+                mouse_capture(true);
             }
             break;
         }
         default: break;
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
+        SDL_RenderClear(g_renderer);
 
         if (g_state==GS_PLAYING||g_state==GS_DEAD||g_state==GS_LEVEL_COMPLETE) {
-            renderer_draw_frame(renderer, g_player);
-            hud_draw(renderer, g_player, g_fps, g_frametime, g_state);
+            renderer_draw_frame(g_renderer, g_player);
+            hud_draw(g_renderer, g_player, g_fps, g_frametime, g_state);
         }
 
         if (g_state==GS_MENU||g_state==GS_PAUSED) {
             if (g_state==GS_PAUSED) {
-                renderer_draw_frame(renderer, g_player);
-                SDL_SetRenderDrawColor(renderer,0,0,0,120);
-                SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+                renderer_draw_frame(g_renderer, g_player);
+                SDL_SetRenderDrawColor(g_renderer,0,0,0,140);
+                SDL_SetRenderDrawBlendMode(g_renderer,SDL_BLENDMODE_BLEND);
                 SDL_Rect full={0,0,SCREEN_W,SCREEN_H};
-                SDL_RenderFillRect(renderer,&full);
+                SDL_RenderFillRect(g_renderer,&full);
             }
-            menu_render(renderer, delta);
+            menu_render(g_renderer, delta);
         }
 
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(g_renderer);
     }
 
+    mouse_capture(false);
     audio_shutdown();
     renderer_destroy();
     hud_shutdown();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
     SDL_Quit();
     return 0;
 }
